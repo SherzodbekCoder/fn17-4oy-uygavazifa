8877a630d535907b93e388b10c43cf054f7ac1c9
asosiymenu007 <asosiymenu007@gmail.com>
2024-02-02 11:58:17 +0500

    homework

---
+++ main.js
@@ -0,0 +1,56 @@
+//1-masala
+// let arr = [1, 12, 2, 22, 3, 21, 32]
+
+// function yangiMassiv(arr) {
+//     let result = arr.filter(num => num > 10);
+//     return result
+// }
+// console.log(yangiMassiv(arr));
+
+
+//2-masala
+// let arr = [1, 2, 3, 4, 5, 6, 7]
+// function kublaridanIYM(arr) {
+//     let res = arr.map(num => num **3)
+//     return res
+// }
+// console.log(kublaridanIYM(arr));
+
+
+//3-masala
+// let mas = [30, 25, 45, 33];
+
+// function karrali(mas) {
+
+//     for (let i = 0; i < mas.length; i++) {
+//         if (mas[i] % 3 == 0 && mas[i] % 5 == 0) {
+//             return true;
+//         } else
+//             return false
+//     }
+
+// }
+// console.log(karrali(mas));
+
+
+
+//4-masala
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// function kottasiniT(arr) {
+//     let topuvchi = arr.sort((a, b) => b - a);
+//     return topuvchi.slice(0,3)
+// }
+// console.log(kottasiniT(arr));
+
+
+
+//5-masala
+// let mas = [1, 2, 3, 4, 5]
+// let arr = mas.reverse().map((v) => {
+
+//     return v.toString()
+// })
+
+// console.log(arr);
+
